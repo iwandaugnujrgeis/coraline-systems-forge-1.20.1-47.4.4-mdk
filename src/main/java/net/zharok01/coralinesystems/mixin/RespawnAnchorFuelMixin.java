56bc5a65b7bbb3b1c38236e49e2cf04580bc09c1
zharok_01 <maxymmosolov@gmail.com>
2026-03-19 15:09:56 +0300

    Implemented Soumeh's Respawn Anchor mixin tweaks

--- src/main/java/net/zharok01/coralinesystems/mixin/RespawnAnchorFuelMixin.java
+++ src/main/java/net/zharok01/coralinesystems/mixin/RespawnAnchorFuelMixin.java
@@ -1,26 +1,18 @@
 package net.zharok01.coralinesystems.mixin;
 
+import com.github.alexthe666.alexsmobs.item.AMItemRegistry;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.RespawnAnchorBlock;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RespawnAnchorBlock.class)
 public class RespawnAnchorFuelMixin {
 
-    @Inject(method = "isRespawnFuel", at = @At("HEAD"), cancellable = true)
-    private static void overrideFuel(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        Item soulHeart = ForgeRegistries.ITEMS.getValue(new ResourceLocation("alexsmobs", "soul_heart"));
-
-        if (soulHeart != null && stack.is(soulHeart)) {
-            cir.setReturnValue(true);
-        }
-
-        else { cir.setReturnValue(false); }
+    @WrapMethod(method = "isRespawnFuel")
+    private static boolean isRespawnFuel(ItemStack stack, Operation<Boolean> original) {
+        if (stack.is(AMItemRegistry.SOUL_HEART.get())) return true;
+        return original.call(stack);
     }
 }
