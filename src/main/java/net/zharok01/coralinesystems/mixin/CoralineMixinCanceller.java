9c28a6ae83a246da9efc9ba76ee24d7c52876c63
zharok_01 <maxymmosolov@gmail.com>
2026-03-19 15:07:52 +0300

    Implemented Soumeh's changes to MixinCanceller

--- src/main/java/net/zharok01/coralinesystems/mixin/CoralineMixinCanceller.java
+++ src/main/java/net/zharok01/coralinesystems/mixin/CoralineMixinCanceller.java
@@ -5,30 +5,17 @@ import com.bawnorton.mixinsquared.api.MixinCanceller;
 import java.util.List;
 
 public class CoralineMixinCanceller implements MixinCanceller {
+
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        if (mixinClassName.equals("com.ordana.spelunkery.mixins.MineshaftMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("net.mehvahdjukaar.supplementaries.mixins.StrongholdRoomSconceMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("net.mehvahdjukaar.supplementaries.mixins.StrongholdCrossingSconceMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("com.teamabnormals.caverns_and_chasms.core.mixin.CreeperMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("net.lerariemann.infinity.mixin.core.NetherPortalBlockMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("net.lerariemann.infinity.mixin.core.NetherPortalMixin")) {
-            return true;
-        }
-        if (mixinClassName.equals("com.faboslav.friendsandfoes.common.mixin.StrongholdGeneratorMixin")) {
-            return true;
-        }
-
-        return false;
+        return switch (mixinClassName) {
+            case "com.ordana.spelunkery.mixins.MineshaftMixin",
+                 "net.mehvahdjukaar.supplementaries.mixins.StrongholdRoomSconceMixin",
+                 "net.mehvahdjukaar.supplementaries.mixins.StrongholdCrossingSconceMixin",
+                 "com.teamabnormals.caverns_and_chasms.core.mixin.CreeperMixin",
+                 "net.lerariemann.infinity.mixin.core.NetherPortalBlockMixin",
+                 "net.lerariemann.infinity.mixin.core.NetherPortalMixin" -> true;
+            default -> false;
+        };
     }
 }
