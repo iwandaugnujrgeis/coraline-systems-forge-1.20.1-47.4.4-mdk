5e91edf85227d8f6de0dc4051d66891dedebfd60
zharok_01 <maxymmosolov@gmail.com>
2026-03-19 12:53:04 +0300

    Tweaked Jerboa's mixin

--- src/main/java/net/zharok01/coralinesystems/mixin/JerboaEffectMixin.java
+++ src/main/java/net/zharok01/coralinesystems/mixin/JerboaEffectMixin.java
@@ -2,26 +2,25 @@ package net.zharok01.coralinesystems.mixin;
 
 import com.github.alexthe666.alexsmobs.entity.EntityJerboa;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(value = EntityJerboa.class, remap = false)
 public class JerboaEffectMixin {
 
-    @Redirect(
-            method = "Lcom/github/alexthe666/alexsmobs/entity/EntityJerboa;m_6071_(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;",
+    @Unique private static final int FLEET_FOOTED_DURATION = 200;
+
+    @ModifyArg(
+            method = "mobInteract",
             at = @At(
-                    value = "NEW",
-                    target = "net/minecraft/world/effect/MobEffectInstance"
-            )
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/effect/MobEffectInstance;<init>(Lnet/minecraft/world/effect/MobEffect;I)V"
+            ),
+            index = 1
     )
-    private net.minecraft.world.effect.MobEffectInstance coraline$changeFleetFootedDuration(
-            net.minecraft.world.effect.MobEffect effect,
-            int originalDuration
-    ) {
-        return new net.minecraft.world.effect.MobEffectInstance(
-                com.github.alexthe666.alexsmobs.effect.AMEffectRegistry.FLEET_FOOTED.get(),
-                200
-        );
+    private int changeFleetFootedDuration(int duration) {
+        return FLEET_FOOTED_DURATION;
     }
+
 }
