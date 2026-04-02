5e60d638e2558153c2071213b949d306157803bc
zharok_01 <maxymmosolov@gmail.com>
2026-04-02 22:41:57 +0300

    Giant tweaks

--- src/main/java/net/zharok01/coralinesystems/mixin/GiantMixin.java
+++ src/main/java/net/zharok01/coralinesystems/mixin/GiantMixin.java
@@ -1,9 +1,12 @@
 package net.zharok01.coralinesystems.mixin;
 
+import com.legacy.rediscovered.entity.pigman.PigmanEntity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+//import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Giant;
 import net.minecraft.world.entity.monster.Monster;
@@ -14,6 +17,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Objects;
+
 @Mixin(Giant.class)
 public abstract class GiantMixin extends Monster {
 
@@ -21,13 +26,17 @@ public abstract class GiantMixin extends Monster {
         super(type, level);
     }
 
-    // 1. Give the Giant a Brain!
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onInit(EntityType<? extends Giant> type, Level level, CallbackInfo ci) {
-        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 16.0F));
-        this.goalSelector.addGoal(8, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
 
+        Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(0.25D);
+
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 32.0F));
+        this.goalSelector.addGoal(8, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        //this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, PigmanEntity.class, true));
     }
 }
