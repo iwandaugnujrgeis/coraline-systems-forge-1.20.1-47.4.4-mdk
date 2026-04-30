349e2e51a97b07bc2b84c1c2abb504d06f759de3
zharok_01 <maxymmosolov@gmail.com>
2026-04-30 12:25:02 +0300

    New Advancements triggers

--- src/main/java/net/zharok01/coralinesystems/registry/CoralineTriggers.java
+++ src/main/java/net/zharok01/coralinesystems/registry/CoralineTriggers.java
@@ -10,10 +10,14 @@ public class CoralineTriggers {
     public static final AMAdvancementTrigger CORRUPT_PORTAL = new AMAdvancementTrigger(new ResourceLocation(CoralineSystems.MOD_ID, "corrupt_portal"));
     public static final AMAdvancementTrigger COBALT_PANTS = new AMAdvancementTrigger(new ResourceLocation(CoralineSystems.MOD_ID, "cobalt_pants"));
     public static final AMAdvancementTrigger HELPER_DANCING = new AMAdvancementTrigger(new ResourceLocation(CoralineSystems.MOD_ID, "helper_dancing"));
+    public static final AMAdvancementTrigger MINESHAFT_DISCOVERED = new AMAdvancementTrigger(new ResourceLocation(CoralineSystems.MOD_ID, "mineshaft_discovered"));
+    public static final AMAdvancementTrigger SCARE_MONSTER = new AMAdvancementTrigger(new ResourceLocation(CoralineSystems.MOD_ID, "scare_monster"));
 
     public static void init() {
         CriteriaTriggers.register(CORRUPT_PORTAL);
         CriteriaTriggers.register(COBALT_PANTS);
         CriteriaTriggers.register(HELPER_DANCING);
+        CriteriaTriggers.register(MINESHAFT_DISCOVERED);
+        CriteriaTriggers.register(SCARE_MONSTER);
     }
 }
