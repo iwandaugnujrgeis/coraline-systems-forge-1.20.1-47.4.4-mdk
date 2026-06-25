36115b002420a080f126fe6d4fbcdea7d2a5e699
zharok_01 <maxymmosolov@gmail.com>
2026-06-25 23:27:50 +0300

    Fixed Zombie Piglins' spawns using ForgeEvents, removed InControl mod

--- src/main/java/net/zharok01/coralinesystems/event/CoralineModEvents.java
+++ src/main/java/net/zharok01/coralinesystems/event/CoralineModEvents.java
@@ -1,6 +1,9 @@
 package net.zharok01.coralinesystems.event;
 
+import com.legacy.rediscovered.registry.RediscoveredEntityTypes;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
@@ -10,9 +13,25 @@ import net.zharok01.coralinesystems.CoralineSystems;
 import net.zharok01.coralinesystems.entity.OrbEntity;
 import net.zharok01.coralinesystems.registry.IsotopicEntities;
 
-// CRITICAL FIX: Added 'bus = Mod.EventBusSubscriber.Bus.MOD'
 @Mod.EventBusSubscriber(modid = CoralineSystems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CoralineModEvents {
+
+    @SubscribeEvent
+    public static void onSpawnPlacementRegister(SpawnPlacementRegisterEvent event) {
+
+        // Patching Rediscovered: They forgot to register a placement for their Zombie Pigman,
+        // causing it to spawn mid-air. We assign the Vanilla Zombified Piglin placement rules here.
+        event.register(
+                RediscoveredEntityTypes.ZOMBIE_PIGMAN,
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                // Lambda wrapper to satisfy Java's strict generic types
+                (type, level, spawnType, pos, random) -> ZombifiedPiglin.checkZombifiedPiglinSpawnRules(
+                        EntityType.ZOMBIFIED_PIGLIN, level, spawnType, pos, random
+                ),
+                SpawnPlacementRegisterEvent.Operation.REPLACE
+        );
+    }
 
     // Registering Attributes
     @SubscribeEvent
